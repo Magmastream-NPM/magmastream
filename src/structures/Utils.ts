86c47d62c61e8fd597a1806864ea8f5bedc75983
Vexify <painmain.shadow@gmail.com>
2025-01-22 02:03:55 +0100

    2.7.5
    - Added missing "alternativePlatforms" for autoPlay
    - Added new changeType to QueueChangeEvent of PlayerStateUpdate (autoPlayAdd), which emits when the autoplay adds a track to the queue
    - Added new changeType to TrackChangeEvent of PlayerStateUpdate (autoPlay), which emits when autoPlay is playing a song instead of the normal "start" changeType

--- src/structures/Utils.ts
+++ src/structures/Utils.ts
@@ -1,4 +1,3 @@
- 
 /* eslint-disable @typescript-eslint/no-unused-vars, @typescript-eslint/no-require-imports */
 import { ClientUser, User } from "discord.js";
 import { Manager } from "./Manager";
