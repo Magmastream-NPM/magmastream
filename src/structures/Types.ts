0a74001027fab89fa65a934638382f69093504f3
Vexify4103 <painmain.shadow@gmail.com>
2025-07-30 15:20:38 +0200

    chore: removed unused PlayerUpdate interface

--- src/structures/Types.ts
+++ src/structures/Types.ts
@@ -797,24 +797,6 @@ export interface SponsorBlockChaptersLoaded extends PlayerEvent {
 	}[];
 }
 
-/**
- * PlayerUpdate interface
- */
-export interface PlayerUpdate {
-	op: "playerUpdate";
-	/** The guild id of the player. */
-	guildId: string;
-	state: {
-		/** Unix timestamp in milliseconds. */
-		time: number;
-		/** The position of the track in milliseconds. */
-		position: number;
-		/** Whether Lavalink is connected to the voice gateway. */
-		connected: boolean;
-		/** The ping of the node to the Discord voice server in milliseconds (-1 if not connected). */
-		ping: number;
-	};
-}
 
 /**
  * NodeStats interface
